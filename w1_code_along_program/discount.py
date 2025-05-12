84b418b1d2d99d3c64030001a75111b7ff5a57ec
Rodrigo-dev260 <rodrigo_stringari@hotmail.com>
2025-05-12 13:44:39 -0300

    Created discount file

--- w1_code_along_program/discount.py
+++ w1_code_along_program/discount.py
@@ -1,1 +1,12 @@
-subtotal = input('Enter the subtotal:')
+DISCOUNT_RATE=.1
+TAX_RATE=.06
+subtotal=float(input('Enter the subtotal'))
+print(f'Total order {subtotal}')
+discount=subtotal * DISCOUNT_RATE
+print(f'Discount {discount}')
+subtotal -=discount
+tax=subtotal * TAX_RATE
+total=subtotal + tax
+print(f'Subtotal {subtotal}')
+print(f'tax {tax}')
+print(f'Total Due {total}')
