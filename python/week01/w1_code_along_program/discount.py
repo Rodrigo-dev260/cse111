38c13ab9f5f4f2d9ea1ce544f411dbae5ffde009
Rodrigo-dev260 <rodrigo_stringari@hotmail.com>
2025-11-13 12:21:15 -0300

    Back to class

--- python/week01/w1_code_along_program/discount.py
+++ python/week01/w1_code_along_program/discount.py
@@ -1,7 +1,11 @@
-DSCOUNT_RATE=.1
-TAX_RATE=.06
-subtotal=input('Enter thesubtotal: ')
-tax=subtotal * TAX_RATE
-total=subtotal + tax
-print(f'Subtotal {subtotal}')
+DISCOUNT_RATE = .1
+TAX_RATE = .06
+subtotal = float(input('Enter the subtotal: '))
+print(f'Total order {subtotal}')
+discount = subtotal * DISCOUNT_RATE
+print(f'Discount {discount}')
+subtotal -= discount
+tax = subtotal * TAX_RATE
+total = subtotal + tax
 print(f'Tax {tax}')
+print(f'Total Due {total}')
